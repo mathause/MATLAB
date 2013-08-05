6e4efc5c87717d82a516824b92eafc4fb8d9054d
Mathias Hauser <mah@meteoswiss.ch>
2013-08-05 09:16:23 +0200

    added help to psel

--- General/datafun/psel.m
+++ General/datafun/psel.m
@@ -1,10 +1,17 @@
 function perc = psel(sel)
-
+%PSEL returns percentage of elements that are true in a logical array
+%
+%Syntax
+%   perc = PSEL(sel)
+%
+%Usage
+%   perc = PSEL(sel) returns percentage of true in the logical array sel
+%
+%Version History
+%   17.07.2013  mah     created
+%
+%See Also
+%   logical | true | sum | size
 
 perc = sum(sel) / size(sel, 1);
-
-
-
-
-
 end
