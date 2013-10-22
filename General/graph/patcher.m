50da75bb1f03aa85b542af0e98096e31e78414d6
Mathias Hauser <mah@meteoswiss.ch>
2013-10-22 11:04:54 +0200

    patcher: allow for NaNs

--- General/graph/patcher.m
+++ General/graph/patcher.m
@@ -13,6 +13,8 @@ validateattributes(high,{'numeric'},{'vector'})
 assert(isequal(size(t), size(low), size(high)), 'patcher:notEqInp', ...
     'The input needs to have the same size/ dimensions')
 
+
+
 if size(t,1) ~= 1
     t = t';
     low = low';
@@ -28,7 +30,24 @@ if nargin < 5 || isempty( inv )
 end
     
     
-
+sel = isnan(low) | isnan(high);
+pt = find(sel);
+if ~isempty(pt)
+   pt = [0, pt];
+   pt = [pt, length(t) + 1]; 
+   for ii = 1:length(pt) - 1
+       IDX = pt(ii)+1:pt(ii+1)-1;
+       
+    if ~isempty(IDX)
+       if nargout == 0
+           patcher( t(IDX), low(IDX), high(IDX), C, inv, varargin{:} );
+       else
+           h = patcher( t(IDX), low(IDX), high(IDX), C, inv, varargin{:} );
+       end
+    end
+   end
+   return
+end
 
 t = [t fliplr(t) t(1)];
 val = [low, fliplr(high), low(1)];
