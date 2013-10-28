3fb22bc0b776914b6807c2cee9b825b2f0db217e
Mathias Hauser <mah@meteoswiss.ch>
2013-10-28 12:20:17 +0100

    boxplots/boxstats buxfixes
    both: accept w = 0
    boxplots: remove ghost points, fix h, updated doc, empty outliers

--- General/datafun/boxstats.m
+++ General/datafun/boxstats.m
@@ -1,31 +1,41 @@
-function [med, p25, p75, whisL, whisH, outlL, outlH ] =  boxstats( X, w )
+function [med, p25, p75, whisL, whisU, outlL, outlU] =  boxstats(X, w)
 %BOXSTATS returns statistics used in boxplots
 %
 %Syntax
-%   [med, p25, p75, whisL, whisH] =  BOXSTATS( X, w )
-%   [med, p25, p75, whisL, whisH, outlL, outlH ] =  BOXSTATS( X, w )
+%   [med, p25, p75] =  BOXSTATS(X)
+%   [med, p25, p75, whisL, whisU] =  BOXSTATS(X)
+%   [med, p25, p75, whisL, whisU] =  BOXSTATS(X, w)
+%   [med, p25, p75, whisL, whisU, outlL, outlU] =  BOXSTATS(...)
 %
 %Usage
-%   [med, p25, p75, whisL, whisH] =  BOXSTATS( X, w ) returns median, 25th
-%       and 75th percentile, and the position of the upper and lower whisker
-%       as used by boxplots. X is the data (2d vector) and w (optional,
-%       scalar) the length of the whiskers (default = 1.5), see below;
-%   [med, p25, p75, whisL, whisH, outlL, outlH ] =  BOXSTATS( X, w ) dito,
-%      but does not return outliers.
+%   [med, p25, p75] =  BOXSTATS(X) returns median, 25th and 75th percentile
+%       of the data vector X (2d vector).
+%   [med, p25, p75, whisL, whisU] =  BOXSTATS(X) also retutns the position
+%       of the upper and lower whisker as used by boxplots. The whiskers 
+%       have the length of w = 1.5, see below.
+%   [med, p25, p75, whisL, whisU] =  BOXSTATS(X, w), also taking the length
+%       of the whiskers as input, (default = 1.5), see below.
+%   [med, p25, p75, whisL, whisU, outlL, outlU] =  BOXSTATS(...) returns 
+%       position of the outliers (all points beyond whisL and whisU).
 %
 %Whiskers (from the boxplot documentation)
 %   Maximum whisker length w. The default is a w of 1.5. Points are drawn
-%   as outliers if they are larger than q3 + w(q3 – q1) or smaller than
-%   q1 – w(q3 – q1), where q1 and q3 are the 25th and 75th percentiles,
+%   as outliers if they are larger than p75 + w(p75 – p25) or smaller than
+%   p25 – w(p75 – p25), where p25 and p75 are the 25th and 75th percentiles,
 %   respectively. The default of 1.5 corresponds to approximately +/–2.7?
 %   and 99.3 coverage if the data are normally distributed. The plotted
 %   whisker extends to the adjacent value, which is the most extreme data
-%   value that is not an outlier. Set whisker to 0 to give no whiskers and
-%   to make every point outside of q1 and q3 an outlier.
+%   value that is not an outlier. If w is 0 whisU and whisL are empty ([]).
+%
+%Examples
+%   [med, p25, p75, whisL, whisU] =  BOXSTATS(rand(50,1), 1);
+%   [med, p25, p75, whisL, whisU, outlL, outlU] =  BOXSTATS(rand(100,1));
 %
 %Version History
 %   16.07.2013  mah     created
 %   23.07.2013  mah     added doc and input checking
+%   28.10.2013  mah     handling of w = 0
+%   
 %
 %See Also
 % boxplots | boxplot | beanplot
@@ -35,11 +45,8 @@ validateattributes(X, {'double', 'single'}, {'2d', 'real'}, mfilename, 'X')
 if nargin < 2;
     w = 1.5;
 else
-    validateattributes(w, {'double'}, {'scalar', 'positive'}, mfilename, 'w')
+    validateattributes(w, {'double'}, {'scalar', 'nonnegative'}, mfilename, 'w')
 end
-
-
-
 
 pc = prctile(X, [25; 50; 75]);
 
@@ -48,29 +55,38 @@ p25 = pc(1,:); p75 = pc(3,:);
 
 
 
-
-
-%determination of the WHISKERS -> see help of boxplot
 if nargout > 3
-    %inter quartile range
-    iqr = diff([p25; p75]);
-
-    whisH = p75 + w*iqr;
-    
-    d = whisH - X;
-    sel = d > 0;
-    whisH = -min(d(sel)) + whisH;
-    
-    whisL = p25 - w*iqr;
-    d = whisL - X;
-    sel = d < 0;
-    whisL = -max(d(sel)) + whisL;
+    if w == 0 %return empty whiskers for w = 0
+        whisL = [];
+        whisU = [];
+    else %determination of the WHISKERS -> see help of boxplot
+        %inter quartile range
+        iqr = diff([p25; p75]);
+        
+        whisU = p75 + w*iqr; %theoretical upper whiskers
+        
+        d = whisU - X; %find next smaller datapoint
+        sel = d > 0;
+        whisU = -min(d(sel)) + whisU;
+        
+        whisL = p25 - w*iqr;
+        d = whisL - X;
+        sel = d < 0;
+        whisL = -max(d(sel)) + whisL;
+    end
 end
+
+
 
 %get the outliers
 if nargout > 5
-    outlL = X(X < whisL);
-    outlH = X(X > whisH);
+    if w == 0
+        outlL = X(X < p25);
+        outlU = X(X > p75);
+    else
+        outlL = X(X < whisL);
+        outlU = X(X > whisU);
+    end
 end
 
 
--- General/graph/boxplots.m
+++ General/graph/boxplots.m
@@ -1,16 +1,89 @@
-function [ h, AX ] = boxplots( varargin )
-%BOXPLOTS Summary of this function goes here
-%   Detailed explanation goes here
+function [ h, AX ] = boxplots(varargin)
+%BOXPLOTS alternative to the MATLAB boxplot
+%
+%Syntax
+%   BOXPLOTS(X)
+%   BOXPLOTS(X, POS)
+%   BOXPLOTS(..., Name, Value, ...)
+%   BOXPLOTS(axes_handle, ...)
+%   h = BOXPLOTS(...)
+%   [h, AX] = BOXPLOTS(...)
+%
+%Usage
+%   BOXPLOTS(X) makes a boxplots with the values in X. X must be a column
+%       vector or a matrix and each column gets a boxplot.
+%   BOXPLOTS(X, POS) makes a boxplot at the position(s) in POS. POS must be
+%       integers and have the same number of elements than X has columns.
+%       If POS is not given, POS is 1:n_columns.
+%   BOXPLOTS(..., 'Name', Value, ...) manipulate the boxplot by setting its
+%       properties (see below).
+%   BOXPLOTS(axes_handle, ...) plots using axes with the handle axes_handle
+%       instead of the current axes (gca).
+%   h = BOXPLOTS(...) returns the handles of the elements of the boxplot.
+%       h is a [8 x n_columns] matrix (see Tipps below).
+%   [h, AX] = BOXPLOTS(...) also returns the handle of the axes.
+%
+%Input
+%Name Value Pairs
+%   compact: {false} | true
+%       plot a compact version of the boxplots
+%   orientation: {'vertical'} | 'horizontal
+%       plot the boxplots horizontally or vertically
+%   outliers: {false} | true
+%       plot outliers or not
+%   whisker: 1.5 | nonnegative number
+%       see below
+%
+%Whiskers (from the MATLAB boxplot documentation)
+%   Maximum whisker length w. The default is a w of 1.5. Points are drawn
+%   as outliers if they are larger than p75 + w(p75 – p25) or smaller than
+%   p25 – w(p75 – p25), where p25 and p75 are the 25th and 75th percentiles,
+%   respectively. The default of 1.5 corresponds to approximately +/–2.7?
+%   and 99.3 coverage if the data are normally distributed. The plotted
+%   whisker extends to the adjacent value, which is the most extreme data
+%   value that is not an outlier. Set whisker to 0 to give no whiskers and
+%   to make every point outside of p25 and p75 an outlier.
+%
+%Output
+%   h   The [8 x n_columns] lists the handles for each drawn object, if an
+%       object is not drawn (e.g. if the outliers are supressed), 0 is
+%       given:
+%   h = {'median';        ... median line
+%        'patch';         ... patch from p25 to p75
+%        'whisL paralel'; ... line from first non-outlier to p25
+%        'whisU paralel'; ... line from p75 ti last non-outlier
+%        'whisL perp';    ... line at first non-outlier
+%        'whisU perp';    ... line at last non-outlier
+%        'outlL';         ... lower outliers
+%        'outlU'}         %   upper outliers
+%
+%Example
+%   x1 = normrnd(5,1,100,1);
+%   x2 = normrnd(6,1,100,1);
+%   boxplots(x1,1)
+%   boxplots(x2,2)
+%
+%   figure
+%   X = randn(100,25);
+%   boxplots(X,'compact', true)
 %
 %Tipps
-%   in order to set the labels use (or 'YTickLabel')
-%   set(gca,'XTickLabel', Labels)
+%   Labels
+%       in order to set the labels use (or 'YTickLabel')
+%       set(gca,'XTickLabel', Labels)
 %
-%   to display a legend use:
-%   h = BOXPLOTS(...)
-%   h_leg = [h(1), h(2) h(3)];
-%   leg = {'Median', 'IQR', 'Range'};
-%   legend(h_leg, leg)
+%   Legend
+%      to display a legend use:
+%       h = BOXPLOTS(...)
+%       h_leg = [h(1), h(2) h(3)];
+%       leg = {'Median', 'IQR', 'Range'};
+%       legend(h_leg, leg)
+%
+%Version History
+%   16.07.2013   mah@MCH   created
+%   26.08.2013   mah@MCH   documentation
+%   28.10.2013   mah@MCH   bugfixes (w=0), doc, ghost pts, empty outliers
+%
 %See Also
 %boxplot | boxstats
 
@@ -24,145 +97,125 @@ is_compact = opt.compact;
 plot_horz = strcmp(opt.orientation, 'horizontal');
 plot_outliers = opt.outliers;
 whisker = opt.whisker;
-
 %end input parsing
-
 
 if is_compact
     patch_col = 'b';
     patch_half_width = 0.33;
+    patch_FaceAlpha = 1;
+
     line_width = 2;
     
     whis_lineSpec = 'b-';
 else
-    patch_half_width = 0.33;    
     patch_col = 'w';
+    patch_half_width = 0.33;
+    patch_FaceAlpha = 1;
+
     line_width = 1;
     
     whis_lineSpec = 'k--';
 end
 
-kk = 1; %for the handles
+hi = zeros(8, n_plots); %handles of the plotted objects
 
-hi = zeros(8, n_plots);
+is_hold = ishold;
 
-hold on
+if ~is_hold
+    plot(NaN); %get rid of previous plots (default MATLAB behaviour)
+    hold on
+end
+
 for ii = 1:n_plots
     %create hggroup to contain all plot elements
     hg = hggroup('Parent',AXi);
-
+    
     if plot_outliers
-        %get statistics incl ouliers
-        [med, p25, p75, whisL, whisH, outlL, outlH] =  boxstats( X(:,ii), whisker );
+        %get statistics incl. ouliers
+        [med, p25, p75, whisL, whisU, outlL, outlU] =  boxstats(X(:,ii), whisker);
         %plot outliers
-        plt = swap_xy( POS(ii)*ones(length(outlL),1), outlL, plot_horz );
-        hi(7,kk) = plot(plt{:}, 'or', 'Tag', 'Low Outliers', 'Parent', hg);
-        plt = swap_xy( POS(ii)*ones(length(outlH),1), outlH, plot_horz );
-        hi(8,kk) = plot(plt{:}, 'or', 'Tag', 'High Outliers', 'Parent', hg);
+        if ~isempty(outlL)
+            hi(7,ii) = swap_plot(plot_horz, POS(ii)*ones(length(outlL),1), outlL, 'or', 'Low Outliers');
+        end
+        if ~isempty(outlU)
+            hi(8,ii) = swap_plot(plot_horz, POS(ii)*ones(length(outlU),1), outlU, 'or', 'High Outliers');
+        end
     else
         %get statistics w/o outliers
-        [med, p25, p75, whisL, whisH] =  boxstats( X(:,ii), whisker );
+        [med, p25, p75, whisL, whisU] =  boxstats(X(:,ii), whisker);
     end
-
-
-%plot the box with the iqr
-x_iqr = [-1 -1 1 1]*patch_half_width + POS(ii);
-y_iqr =  [ p25   p75  p75  p25];
-plt = swap_xy( x_iqr,y_iqr, plot_horz );
-hi(2, kk) = patch(plt{:},patch_col, 'EdgeColor','b', 'Tag', 'Box', 'Parent', hg);
-
-
-%plot the median
-x_med = x_iqr(2:3);
-plt = swap_xy( x_med, [med med], plot_horz );
-hi(1, kk) = plot(plt{:} , 'r', 'LineWidth', line_width, 'Tag', 'Median', 'Parent', hg);
-
-%plot the whiskers
-x_whis_vert = [0 0] + POS(ii);
-plt = swap_xy( x_whis_vert, [p25 whisL], plot_horz );
-hi(3, kk) = plot(plt{:}, whis_lineSpec, 'LineWidth', line_width, 'Tag', 'Lower Whisker', 'Parent', hg);
-plt = swap_xy( x_whis_vert, [p75 whisH], plot_horz );
-hi(4, kk) = plot(plt{:}, whis_lineSpec, 'LineWidth', line_width, 'Tag', 'Higher Whisker', 'Parent', hg);
     
-%plot the horizontal whiskers (lower adjacent)
-if ~is_compact   
-    x_whis_horz = [-0.25 0.25] + POS(ii);
-    plt = swap_xy( x_whis_horz, [whisL, whisL], plot_horz );
-    hi(5, kk) = plot(plt{:}, 'k', 'Tag', 'Lower Adjacent Value', 'Parent', hg);
-    plt = swap_xy( x_whis_horz, [whisH, whisH], plot_horz );
-    hi(6, kk) = plot(plt{:}, 'k', 'Tag', 'Upper Adjacent Value', 'Parent', hg);
-end
-
-
-%plot 2 ghost points to get a bit a nicer ylim (dont set them explicitly
-%because you would need to get all the previous ones when adding a boxplot)
-plt = swap_xy( POS(ii), whisL - abs(whisL)*0.05, plot_horz );
-plot(plt{:}, 'w', 'Tag', 'Lower Ghost', 'Parent', hg )
-plt = swap_xy( POS(ii), whisH + abs(whisL)*0.05, plot_horz );
-plot(plt{:}, 'w', 'Tag', 'Upper Ghost', 'Parent', hg)
-
-
-
-end
-hold off
-
-
-if plot_horz
- ax = 'Y';
     
-else
-    ax = 'X';
+    %plot the box with the iqr
+    x_iqr = [-1 -1 1 1]*patch_half_width + POS(ii);
+    y_iqr =  [p25   p75  p75  p25];
+    plt = swap_xy(x_iqr,y_iqr, plot_horz);
+    hi(2, ii) = patch(plt{:},patch_col, 'EdgeColor','b', 'Tag', 'Box', 'Parent', hg, 'FaceAlpha', patch_FaceAlpha);
+    
+    
+    %plot the median
+    x_med = x_iqr(2:3);    
+    hi(1, ii) = swap_plot(plot_horz, x_med, [med med], 'r', 'Median', 'LineWidth', line_width);
+    
+    
+    if whisker > 0
+        %plot the whiskers
+        x_whis_vert = [0 0] + POS(ii);
+        hi(3, ii) = swap_plot(plot_horz, x_whis_vert, [p25 whisL], whis_lineSpec, 'Lower Whisker',  'LineWidth', line_width);
+        hi(4, ii) = swap_plot(plot_horz, x_whis_vert, [p75 whisU], whis_lineSpec, 'Higher Whisker', 'LineWidth', line_width);
+        
+        
+        %plot the horizontal whiskers (lower adjacent)
+        if ~is_compact
+            x_whis_horz = [-0.25 0.25] + POS(ii);
+            hi(5, ii) = swap_plot(plot_horz, x_whis_horz, [whisL, whisL], 'k', 'Lower Adjacent Value');
+            hi(6, ii) = swap_plot(plot_horz, x_whis_horz, [whisU, whisU], 'k', 'Upper Adjacent Value');
+        end
+    end
 end
+
+if ~is_hold; hold off; end
+
+
+%set Ticks and Lim
+if plot_horz; ax = 'Y'; else ax = 'X'; end
 
 first_plt = strcmp(get(AXi, [ax, 'TickMode']), 'auto');
 if first_plt
     set(AXi, [ax 'Tick'], POS)
-    
     set(AXi, [ax 'Lim'], [floor(POS(1))-1 ceil(POS(end))+1])
-    
 else
     Ticks = get(AXi, [ax 'Tick']);
-    
     Ticks = union(Ticks, POS);
-    
     set(AXi, [ax 'Tick'], Ticks)
-    
-    
     set(AXi, [ax 'Lim'], [floor(Ticks(1)) - 1 ceil(Ticks(end)) + 1])
-    
 end
-    
-    
-% if ~isempty( TickLabel )
-%     if first_plt
-%         set(AX, [ax 'TickLabel'], TickLabel)
-%     else
-%         a = cellstr(get(AX, [ax 'TickLabel']));
-%         [TLabel{ Ticks }] = a{:};
-%         [TLabel{POS}] = TickLabel{:};        
-%         empty_labels = cellfun(@isempty, TLabel);
-%         TLabel(empty_labels) = [];
-%         set(AX, [ax 'TickLabel'], TLabel)         
-%     end
-% else
-% end
 
 box on
 
-%x_lim = get(AX, 'XLim');
+
 if nargout > 0
-    
-   AX = AXi; 
-   h = hi;
+    AX = AXi;
+    h = hi;
 end
 
-
-
+% =========================== NESTED FUNCTIONS ============================
+    %nested function (because of hg)
+    function h = swap_plot(swap, x, y, LineSpec, Tag, varargin)
+        %ploting function with the common settings
+        sw_plt = swap_xy(x, y, swap); %swap xy if necessary
+        if nargout == 0
+            %all plots have a Tag and hg as Parent
+            plot(sw_plt{:}, LineSpec, 'Tag', Tag, 'Parent', hg, varargin{:});
+        else
+            h = plot(sw_plt{:}, LineSpec, 'Tag', Tag, 'Parent', hg, varargin{:}); 
+        end  
+    end
+% ========================= END NESTED FUNCTIONS ==========================
 
 end
 
-
-
+% ============================ LOCAL FUNCTIONS ============================
 function [AX, X, POS, opt] = parseInput(varargin)
 
 [AX,ARGS,NARGS] = axescheck(varargin{:});
@@ -171,7 +224,7 @@ function [AX, X, POS, opt] = parseInput(varargin)
 if isempty(AX)
     AX = gca;
 else
-    %axes(AX)
+    %set AX as current
     f = get(AX, 'Parent');
     set(0, 'currentfigure', f);
     set(f, 'currentaxes', AX);
@@ -181,14 +234,16 @@ X = ARGS{1};
 
 n_plots = size(X, 2);
 
-if numel(X) <= 1
-   error('boxplots:invHandle', 'X must have more than one element (maybe you specified an invalid axes handle).'); 
+if size(X, 1) <= 1
+    error('boxplots:invHandle', 'X must have more than one element (maybe you specified an invalid axes handle).');
 end
 
-
-if NARGS > 1 && isnumeric( ARGS{2} ) && ~isempty( ARGS{2})
+if NARGS > 1 && isnumeric(ARGS{2}) && ~isempty(ARGS{2})
     POS = ARGS{2};
-    assert(all(rem(POS, 1) == 0), 'boxplots:POSnotINT', 'The vector with the positions must only contain integers.'); 
+    assert(all(rem(POS, 1) == 0), 'boxplots:POSnotINT', 'The vector with the positions must only contain integers.');
+    assert(length(POS) == n_plots, 'boxplots:nPOSnot_eq_nPlots', ...
+        'The POS vector must have the same number of elements (has %i) as X has columns (has %i)', ...
+        length(POS), n_plots)
     ARGS(1:2) = [];
 else
     POS = 1:n_plots;
@@ -199,17 +254,10 @@ end
 
 p = inputParser;
 
-%p.KeepUnmatched = true;
-%boxplot
-
 addParamValue(p,'compact',false)
 addParamValue(p,'orientation', 'vertical')
 addParamValue(p,'outliers', false)
 addParamValue(p,'whisker', 1.5);
-
-
-
-
 
 parse(p, ARGS{:})
 
@@ -220,16 +268,14 @@ end
 
 
 
-
-
-
-function plt = swap_xy( x, y, swap )
-
+function plt = swap_xy(x, y, swap)
+%exchange x & y to plot horizontally or vertically
 if swap
     plt{1} = y; plt{2} = x;
 else
     plt{1} = x; plt{2} = y;
 end
-
-
 end
+
+% ========================== END LOCAL FUNCTIONS ==========================
+
