6e74543cac087edf967bd47651063fee59de4f23
Mathias Hauser <mah@meteoswiss.ch>
2013-12-23 09:10:39 +0100

    bk_md5
    -added recursive loop to list all files/folders

--- bk_md5/bk_md5.m
+++ bk_md5/bk_md5.m
@@ -6,12 +6,15 @@ function [ output_args ] = bk_md5( input_args )
 
 
 
-folder =  'C:\Users\mah\Documents\MATLAB';
+%folder =  'C:\Users\mah\Documents\MATLAB';
 
+folder =  'C:\Users\mah\Documents';
 
 files = [];
 
-files = lst_all(folder, files);
+ignore = {}; %{'.git'};
+
+files = lst_all(folder, files, ignore);
 
 
 assb(files)
@@ -20,25 +23,43 @@ assb(files)
 
 end
 
-function files = lst_all(folder, files)
+function files = lst_all(folder, files, ignore)
 
 list = dir(folder);
 
 %get rid of 'folder' ('.') and folder above ('..')
-sel = strcmp({list(:).name}, {'.'}) | strcmp({list(:).name}, {'..'});
+sel = strcmp({list(:).name}, {'.'}) |  strcmp({list(:).name}, {'..'});
 list(sel) = [];
 
+sel = false(size(list))';
 
+for ii = 1:length(ignore);
+    strcmp({list(:).name}, ignore{ii})
+    sel = sel | strcmp({list(:).name}, ignore{ii});
+end
+list(sel) = [];
 
 isdir = [list.isdir];
 
+[list(:).fullname]=deal([]);
+list = list';
 
-files = [files; list(~isdir)];
+
+for ii = find(~isdir)
+    
+   list(ii).fullname = fullfile(folder, list(ii).name); 
+    
+    
+end
+
+
+
+files = [files, list(~isdir)];
 
 pt = find(isdir);
 for ii = pt
     f = fullfile(folder, list(ii).name);
-    files = lst_all(f , files);
+    files = lst_all(f , files, ignore);
 
 end
 
